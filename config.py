2870ed0ce3af6b3ef9985ea9e31e2edeed8a2394
iamvincenzo <vincenzo.fraello@outlook.it>
2024-03-30 11:22:45 +0100

    initial commit for configuration file.

---
+++ config.py
@@ -0,0 +1,7 @@
+config = {
+    "DB_NAME": "langchain_demo",
+    "COLLECTION_NAME": "collection_of_text_blobs",
+    "FILE_PATHS": "./sample_files",
+    "CHUNK_SIZE": 300,
+    "CHUNK_OVERLAP": 100
+}
