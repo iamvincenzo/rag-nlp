86b068c27a7409f5de46d3fb96361d7fb3b6b49b
iamvincenzo <vincenzo.fraello@outlook.it>
2024-04-02 01:59:30 +0200

    update docs

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ config = {
     "DB_NAME": "langchain_demo",
     "COLLECTION_NAME": "collection_of_text_blobs",
     "FILE_PATHS": "./sample_files",
-    "CHUNK_SIZE": 300,
+    "CHUNK_SIZE": 1000,
     "CHUNK_OVERLAP": 100
 }
