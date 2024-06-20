45d2cdcf1a6d87285bf39a7db37435de479931d8
Vincenzo Fraello <43069944+iamvincenzo@users.noreply.github.com>
2024-06-20 11:21:35 +0200

    Update key_param.py

--- key_param.py
+++ key_param.py
@@ -5,4 +5,4 @@ File used to save the OpenAI API key and the MongoDB URI as well.
 MONGO_URI = "mongodb+srv://vincenzofraello1:TWoEJiJ8QJdWOiBb@cluster0.n47qhdk.mongodb.net/" \
             "?retryWrites=true&w=majority&appName=Cluster0"
 
-OPENAI_API_KEY = "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"
+OPENAI_API_KEY = ""
